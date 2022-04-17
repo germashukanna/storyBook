059cf2a98e279edffe506b187c3d90d0971f9a5b
germashukanna <germashuk@gmail.com>
2022-04-17 15:29:27 +0300

    React кабзда как подробно 17

---
+++ my-app/src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+export default {
+    title: "React.memo demo"
+}
+
+const Counter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)
+        }</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Exampl1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem"])
+    const addUsers = () => {
+        const newUser = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUser)
+    }
+    return <>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick={addUsers}>addUser</button>
+        <Counter count={counter}/>
+        <Users users={users}/>
+
+    </>
+}
